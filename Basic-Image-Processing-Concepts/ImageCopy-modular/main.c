d0e33ce9b60971544ca067513b2ac9718b29e6c4
ellenfel <ellfelfen@gmail.com>
2023-02-16 17:16:09 +0300

    debugging

--- Basic-Image-Processing-Concepts/ImageCopy-modular/main.c
+++ Basic-Image-Processing-Concepts/ImageCopy-modular/main.c
@@ -22,51 +22,53 @@ void  imageReader(const char *imgName,
 
 int main()
 {
-    int imgWidth, imgHeight, imgBitDepth;
-    unsigned char imgHeader[BMP_HEADER_SIZE];
-    unsigned char imgColorTable[BMP_COLOR_TABLE_SIZE];
-    unsigned char imgBuffer[CUSTOM_IMG_SIZE];
+   int imgWidth, imgHeight, imgBitDepth;
+   unsigned char imgHeader[BMP_HEADER_SIZE];
+   unsigned char imgColorTable[BMP_COLOR_TABLE_SIZE];
+   unsigned char imgBuffer[CUSTOM_IMG_SIZE];
 
-    const char imgName[] = "images/man.bmp";
-    const char newImgName = "images/man_copy.bmp";
+   const char imgName[] ="images/man.bmp";
+   const char newImgName[] ="images/man_copy.bmp";
 
-    imageReader(imgName,&imgWidth,&imgHeight,&imgBitDepth,&imgHeader[0],&imgColorTable[0], &imgBuffer[0]);
-    ImageWriter(newImgName,imgHeader,imgColorTable,imgBuffer,imgBitDepth);
+   imageReader(imgName,&imgWidth,&imgHeight,&imgBitDepth,&imgHeader[0],&imgColorTable[0],&imgBuffer[0]);
+   imageWriter(newImgName,imgHeader,imgColorTable,imgBuffer,imgBitDepth);
 
-    printf("Success \n");
+   printf("Success !\n");
 
     return 0;
 }
 
-void imageReader(const char *imgName,
-                 int *_height,
-                 int *_width,
-                 int *_bitDepth,
-                 unsigned char *_header,
-                 unsigned char *_colorTable,
-                 unsigned char *_buf)
+
+
+
+void  imageReader(const char *imgName,
+                  int *_height,
+                  int *_width,
+                  int *_bitDepth,
+                  unsigned char *_header,
+                  unsigned char *_colorTable,
+                  unsigned char *_buf
+                  )
 {
     int i;
     FILE *streamIn;
-    streamIn = fopen(imgName, "rb");
+    streamIn = fopen(imgName,"rb");
 
-    if(streamIn == (FILE*)0)
+    if(streamIn ==(FILE *)0)
     {
-        printf("Unable to open the file\n");
+        printf("Unable to read image \n");
     }
 
-    //we are extracting the header data
-    for(i=0;i<54;i++)
+    for(i =0;i<54;i++)
     {
         _header[i] = getc(streamIn);
     }
 
-    *_width = *(int *)&_header[10];
+    *_width = *(int *)&_header[18];
     *_height = *(int *)&_header[22];
     *_bitDepth = *(int *)&_header[28];
-    
-    //Checking for color table
-    if(*_bitDepth <= 50)
+
+    if(*_bitDepth <=8)
     {
         fread(_colorTable,sizeof(unsigned char),1024,streamIn);
     }
@@ -76,21 +78,19 @@ void imageReader(const char *imgName,
     fclose(streamIn);
 }
 
-void ImageWriter(const char *imgName,
+
+void imageWriter(const char *imgName,
                  unsigned char *header,
                  unsigned char *colorTable,
                  unsigned char *buf,
                  int bitDepth)
-{
-    FILE *fo = fopen(imgName,"wb");
-    fwrite(header,sizeof(unsigned char),54,fo);
-    
-    if(bitDepth <= 8)
-    {
-        fwrite(colorTable,sizeof(unsigned char),1024,fo);
-    }
-
-    fwrite(buf,sizeof(unsigned char), CUSTOM_IMG_SIZE, fo);
-    fclose(fo);
-
-}
+   {
+     FILE *fo = fopen(imgName,"wb");
+     fwrite(header,sizeof(unsigned char),54,fo);
+     if(bitDepth <=8)
+     {
+         fwrite(colorTable,sizeof(unsigned char),1024,fo);
+     }
+     fwrite(buf,sizeof(unsigned char),CUSTOM_IMG_SIZE, fo);
+     fclose(fo);
+   }
