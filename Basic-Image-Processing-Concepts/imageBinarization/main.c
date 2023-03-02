481e3bdf2502b2dc125f7cce472c8f8b2eed668c
ellenfel <ellfelfen@gmail.com>
2023-03-02 19:39:37 +0300

    started workin on image binarization

---
+++ Basic-Image-Processing-Concepts/imageBinarization/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BMP_HEADER_SIZE         54
+#define BMP_COLOR_TABLE_SIZE    1024
+#define CUSTOM_IMG_SIZE         1024*1024
+
+int main()
+{
+
+    return 0;
+}
